74e26340a9e2eceb6afce673f7a4219a1687cad3
Tyler <toreskey@gmail.com>
2018-12-03 10:52:14 -0700

    added compose form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Toolbar from '../Toolbar/Toolbar'
+import Message from '../Message/Message'
+import ComposeForm from '../ComposeForm/ComposeForm'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Toolbar />
+        <Message />
+        <ComposeForm />
       </div>
     );
   }
