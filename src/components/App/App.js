2b862a3bfe381788d72deddec3b468d5ddcdd0d2
Tyler <toreskey@gmail.com>
2018-12-03 10:27:12 -0700

    added toolbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Toolbar from '../Toolbar/Toolbar'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Toolbar />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+//import bootstrap and font awesome
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import 'font-awesome/css/font-awesome.css';
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
